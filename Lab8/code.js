a9142a02bc8849be3330553a404bd350e4d90f38
billTas <billtas@gmail.com>
2023-11-21 19:59:05 +0200

    Finish all tasks

--- Lab8/code.js
+++ Lab8/code.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const correct = document.querySelector('.result'); 
     
+    //reret color function 
     function resetColor() {
         console.log('reset')
 
         let elements = document.getElementsByClassName('answers') 
 
         for (let i = 0; i < elements.length; i++)
-            elements[i].style.backgroundColor = "lightgray"    
+            elements[i].style.backgroundColor = "lightcyan"    
     }
 
-    console.log('ready')
+    //Part1 multible choise
     document.querySelectorAll('button').forEach(function(button) {
         button.onclick = () => {
             console.log('click')
-            button.backgroundColor = 'yellow'
+            
             if (button.id === 'correct') {
                 console.log('correct')
                 
@@ -23,10 +24,33 @@ document.addEventListener('DOMContentLoaded', () => {
                 button.style.backgroundColor = 'green'
             }
             else {
+                resetColor()
                 button.style.backgroundColor = 'red'
                 correct.innerHTML = 'Incorrect'
             }
         }
         
     })
+
+    //Part2
+    
+    let confirmBtn = document.querySelector('#confirmBtn')
+    let result2 = document.querySelector('#result2')
+
+    confirmBtn.onclick = () => {
+        let text = document.querySelector('input')
+        console.log(text.value)
+        if(text.value === 'Greece') {
+            console.log("Congratulations")
+            text.style.backgroundColor = 'green'
+            result2.innerHTML = 'Correct'
+        }
+        else {
+            console.log('false')
+            text.style.backgroundColor = "red"
+            result2.innerHTML = 'Incorrect'
+        }
+    }
+    
+
 })
